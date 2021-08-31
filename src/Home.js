5b09cd24d61b48360e5751c158609d1c6bc0eb01
Yash Gadwal <yash7gadwal@gmail.com>
2021-08-31 05:45:27 +0530

    Added 'fetching the data with useEffect' functionality, conditional loading message, and a functionality to handle the fetch errors.

--- src/Home.js
+++ src/Home.js
@@ -4,29 +4,46 @@ import BlogList from "./Bloglist";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
 
-const handleDelete = (id) => {
-    // console.log(blogs);
-    const newBlogs = blogs.filter(blog => blog.id !== id);    
-    setBlogs(newBlogs);
-}
+// const handleDelete = (id) => {
+//     // console.log(blogs);
+//     const newBlogs = blogs.filter(blog => blog.id !== id);    
+//     setBlogs(newBlogs);
+// }
 
 
 useEffect(() => {
-    fetch('  http://localhost:8000/blogs')
-    .then(res => {
-        return res.json();
-    })
-    .then((data) => {
-        setBlogs(data);
-    })    
+    setTimeout(() => {
+        fetch('  http://localhost:8000/blogs')
+        .then(res => {
+            if(!res.ok){
+                throw Error('Could not fetch the data from the database.');
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        // For handling Fetch related errors.
+        .catch(err => { 
+            setIsPending(false);
+            setError(err.message)
+        })
+    }, 1000);
+    
 }, []); // The square brackets (known as empty dependency array) will make sure that the useEffect runs just once on the first render! It won't run when the state changes, else, without the brackets, the useEffect would've run again.
 //The useEffect function will run for the state mentioned in the dependency array!
 
     return ( 
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
+            {error && <div>{error}</div> }
+            { isPending && <div> Loading... </div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Ushma S.')} title="Ushma's Blogs!"/> */}
         </div>
      );
