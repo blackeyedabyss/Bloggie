8a5a02cae0bb61462faecea9c53980cc044a9011
Yash Gadwal <yash7gadwal@gmail.com>
2021-08-31 17:25:34 +0530

    Added links to the individual blog items, so as to go to the blog details, when clicked upon it.

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal})
       .then(res => {
         if (!res.ok) { // error coming back from server
           throw Error('could not fetch the data for the database / data source.');
@@ -15,16 +17,23 @@ const useFetch = (url) => {
         return res.json();
       })
       .then(data => {
-        setIsPending(false);
         setData(data);
+        setIsPending(false);
         setError(null);
       })
       .catch(err => {
+          if(err.name === 'AbortError'){
+              console.log('Fetch Aborted.');
+            }
         // auto catches network / connection error
-        setIsPending(false);
-        setError(err.message);
+        else{
+            setIsPending(false);
+            setError(err.message);
+        }
       })
     }, 1000);
+
+    return() => abortCont.abort();
   }, [url])// The square brackets (known as the dependency array) will make sure that the useEffect runs just once on the first render! It won't run when the state changes, else, without the brackets, the useEffect would've run again.
   //The useEffect function will run for the state mentioned in the dependency array!
 
